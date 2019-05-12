e43d5bd22ab098920a1ed9a9df6594d9c97f6088
zhangzhao <zhangzhao314@qq.com>
2019-05-12 13:11:40 +0800

    添加了获取支付商品的接口

--- moocshop/src/router/index.js
+++ moocshop/src/router/index.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 import GoodsList from '@/views/GoodsList'
 import Cart from '@/views/Cart'
 import Address from '@/views/Address'
+import OrderConfirm from '@/views/OrderConfirm'
 
 Vue.use(Router)
 
@@ -23,6 +24,11 @@ export default new Router({
       path: '/address',
       name: 'Address',
       component: Address
+    },
+    {
+      path: '/orderconfirm',
+      name: 'OrderConfirm',
+      component: OrderConfirm
     }
   ]
 })
