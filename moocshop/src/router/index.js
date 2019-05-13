3ea8ba76633dcede8a27290323a0d31cc408117a
zhangzhao <zhangzhao314@qq.com>
2019-05-13 12:27:19 +0800

    添加了支付成功页面的路由

--- moocshop/src/router/index.js
+++ moocshop/src/router/index.js
@@ -5,7 +5,7 @@ import GoodsList from '@/views/GoodsList'
 import Cart from '@/views/Cart'
 import Address from '@/views/Address'
 import OrderConfirm from '@/views/OrderConfirm'
-
+import OrderSuccess from '@/views/OrderSuccess'
 Vue.use(Router)
 
 export default new Router({
@@ -29,6 +29,11 @@ export default new Router({
       path: '/orderconfirm',
       name: 'OrderConfirm',
       component: OrderConfirm
+    },
+    {
+      path: '/ordersuccess',
+      name: 'OrderSuccess',
+      component: OrderSuccess
     }
   ]
 })
